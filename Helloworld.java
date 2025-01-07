0bb3105cdff1d6a6cf892fefc54a45a8efd8471d
Your Name <harshil0412singh@gmail.com>
2025-01-07 14:01:49 +0530

    abc

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,9 @@
+import java.util.*;
 class Helloworld{
 public static void main(String[]args){
-System.out.println("hello world");
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc=nextInt();
+    System.out.println(a+b);
 }
 }
